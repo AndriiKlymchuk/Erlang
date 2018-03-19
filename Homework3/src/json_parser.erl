df417c70dd16d5827aea0c453242cf1ebecf6894
Andrey Klymchuck <ua.challenger@gmail.com>
2018-03-19 15:51:19 +0200

    Homework3: json_parser:decode with tests added Created generic .gitignore

--- Homework3/src/json_parser.erl
+++ Homework3/src/json_parser.erl
@@ -1,6 +1,148 @@
 -module(json_parser).
 
--export([decode/1]).
+-export([decode/1
+		,decode/2]).
 
+-define(IS_DIGIT(C), C >= $0, C =< $9).
+
+-type format() :: map | proplist.
+-type opt()    :: {format, format()}.
+-type key()    :: binary().
+-type value()  :: binary() | list() | number() | object() | true | false | null.
+-type object() :: #{key() => value()} | [{key(), value()}].
+
+-record(opt, {format=map :: format()}).
+
+-spec(decode(Bin :: binary()) ->
+	object() | {error, badarg}).
 decode(Bin) ->
-	Bin.
+	decode(Bin, []).
+
+-spec(decode(Bin :: binary(), Opt :: list()) ->
+	object() | {error, badarg}).
+decode(Bin, Opt) ->
+	try start_decode(Bin, Opt) of
+		Obj -> Obj
+	catch
+		_:_ -> {error, badarg}
+	end.
+
+-spec(start_decode(Bin :: binary(), Opt :: list()) ->
+	object()).
+start_decode(Bin, Opt) ->
+	<<"{", Rest0/binary>> = trim_left(Bin),
+	{Obj, Rest1} = parse_object(trim_left(Rest0), prep_opt(Opt)),
+	<<>> = trim_left(Rest1),
+	Obj.
+
+-spec(parse_object(binary(), #opt{}) ->
+	{object(), Rest :: binary()}).
+parse_object(<<"}", Rest/binary>>, #opt{format=map})      -> {#{}, Rest};
+parse_object(<<"}", Rest/binary>>, #opt{format=proplist}) -> {[], Rest};
+parse_object(Bin, Opt) -> parse_props(Bin, [], Opt).
+
+-spec(parse_props(binary(), Acc :: list(), #opt{}) ->
+	{object(), Rest :: binary()}).
+parse_props(<<$", Rest0/binary>>, Acc, Opt = #opt{format=Format}) ->
+	{Key = <<_:1/binary, _/binary>>, Rest1} = string(Rest0),
+	<<":", Rest2/binary>> = trim_left(Rest1),
+	{Value, Rest3} = parse_value(trim_left(Rest2), Opt),
+	NewAcc = [{Key, Value}|Acc],
+	case trim_left(Rest3) of
+		<<",", Rest4/binary>> ->
+			parse_props(trim_left(Rest4), NewAcc, Opt);
+		<<"}", Rest4/binary>> ->
+			case Format of
+				map      -> {maps:from_list(NewAcc), Rest4};
+			    proplist ->	{lists:reverse(NewAcc),  Rest4}
+			end
+	end.
+
+-spec(parse_value(binary(), #opt{}) ->
+	{value(), Rest :: binary()}).
+parse_value(<<"false", Rest/binary>>, _) -> {false, Rest};
+parse_value(<<"true", Rest/binary>>, _)  -> {true, Rest};
+parse_value(<<"null", Rest/binary>>, _)  -> {null, Rest};
+parse_value(<<$[, Rest/binary>>, Opt) -> parse_array(trim_left(Rest), Opt);
+parse_value(<<${, Rest/binary>>, Opt) -> parse_object(trim_left(Rest), Opt);
+parse_value(<<$", Rest/binary>>, _)   -> string(Rest);
+parse_value(<<Bin/binary>>, _)        -> number(Bin).
+
+-spec(parse_array(binary(), #opt{}) ->
+	{list(), Rest :: binary()}).
+parse_array(<<"]", Rest/binary>>, _) -> {[], Rest};
+parse_array(<<Bin/binary>>, Opt)     -> parse_array(Bin, [], Opt).
+
+-spec(parse_array(binary(), Acc :: list(), #opt{}) ->
+	{list(), Rest :: binary()}).
+parse_array(<<Bin/binary>>, Acc, Opt) ->
+	{Value, Rest0} = parse_value(Bin, Opt),
+	case trim_left(Rest0) of
+		<<",", Rest1/binary>> ->
+			parse_array(trim_left(Rest1), [Value|Acc], Opt);
+		<<"]", Rest1/binary>> ->
+			{lists:reverse([Value|Acc]), Rest1}
+	end.
+
+-spec(string(binary()) ->
+	{String :: binary(), Rest :: binary()}).
+string(Bin) ->
+	{Len, _} = binary:match(Bin, <<"\"">>),
+	<<String:Len/binary, _, Rest/binary>> = Bin,
+	{String, Rest}.
+
+-spec(number(binary()) ->
+	{number(), Rest :: binary()}).
+number(<<$-, Rest/binary>>) ->
+	number_integer_part(Rest, -1);
+number(Bin) ->
+	number_integer_part(Bin, 1).
+
+-spec(number_integer_part(binary(), Sign :: 1 | -1) ->
+	{number(), Rest :: binary()}).
+number_integer_part(<<$0, Rest/binary>>, Sign) ->
+	number_fraction_part(Rest, 0, Sign);
+number_integer_part(<<C, Rest/binary>>, Sign) when ?IS_DIGIT(C) ->
+	number_integer_part_rest(Rest, C - $0, Sign).
+
+-spec(number_integer_part_rest(binary(), Int :: integer(), Sign :: 1 | -1) ->
+	{number(), Rest :: binary()}).
+number_integer_part_rest(<<C, Rest/binary>>, Int, Sign) when ?IS_DIGIT(C) -> 
+	number_integer_part_rest(Rest, Int * 10 + C - $0, Sign);
+number_integer_part_rest(Bin, Int, Sign) ->
+	number_fraction_part(Bin, Int, Sign).
+
+-spec(number_fraction_part(binary(), Int :: integer(), Sign :: 1 | -1) ->
+	{number(), Rest :: binary()}).
+number_fraction_part(<<$., Rest/binary>>, Int, Sign) ->
+	number_fraction_part_rest(Rest, Int, 0, Sign);
+number_fraction_part(Bin, Int, Sign) ->
+	{Int * Sign, Bin}.
+
+-spec(number_fraction_part_rest(binary(), Num :: number(),
+								DecimalsNum :: non_neg_integer(),
+								Sign :: 1 | -1) ->
+	{float(), Rest :: binary()}).
+number_fraction_part_rest(<<C, Rest/binary>>,
+						  Num, DecimalsNum, Sign) when ?IS_DIGIT(C) ->
+	number_fraction_part_rest(Rest, Num * 10 + C - $0, DecimalsNum + 1, Sign);
+number_fraction_part_rest(Bin, Num, DecimalsNum, Sign) when DecimalsNum > 0 ->
+	{Num / math:pow(10, DecimalsNum) * Sign, Bin}. 
+
+-spec(trim_left(binary()) ->
+	binary()).
+trim_left(<<"\s", Rest/binary>>) -> trim_left(Rest);
+trim_left(<<"\t", Rest/binary>>) -> trim_left(Rest);
+trim_left(<<"\n", Rest/binary>>) -> trim_left(Rest);
+trim_left(<<"\r", Rest/binary>>) -> trim_left(Rest);
+trim_left(Bin)                   -> Bin.
+
+-spec(prep_opt([opt()]) ->
+	#opt{}).
+prep_opt(OptList) -> prep_opt(OptList, #opt{}).
+
+-spec(prep_opt([opt()], #opt{}) ->
+	#opt{}).
+prep_opt([{format, F}|T], Opt) -> prep_opt(T, Opt#opt{format=F});
+prep_opt([_|T], Opt)           -> prep_opt(T, Opt);
+prep_opt([], Opt)              -> Opt.
--- Homework3/test/h3_tests.erl
+++ Homework3/test/h3_tests.erl
@@ -1,6 +1,169 @@
 -module(h3_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-json_parser_decode_test_() ->
-	[].
+json_parser_decode_as_map_test_() ->
+	[
+		?_assertEqual(#{}, json_parser:decode(<<"{}">>)),
+		?_assertEqual(	
+			#{
+				<<"a">> => <<"string">>,
+			  	<<"b">> => true,
+				<<"c">> => false,
+				<<"d">> => null,
+				<<"e">> => [],
+				<<"f">> => #{},
+				<<"g">> => 234,
+				<<"h">> => 56.50,
+				<<"i">> => <<>>,
+				<<"j">> => 0
+			},
+			json_parser:decode(
+				<<"
+				{
+					\"a\":\"string\",
+					\"b\":true,
+					\"c\":false,
+					\"d\":null,
+					\"e\":[],
+					\"f\":{},
+					\"g\":234,
+					\"h\":56.50,
+					\"i\": \"\",
+					\"j\": 0
+				}
+				">>
+			)
+		),
+		?_assertEqual(	
+			#{
+				<<"abc">> => [
+					<<"def">>, true, false, null, [], #{<<"a">> => true}, 0, 0.768
+				],
+				<<"-*-">> => #{
+					<<"a">> => <<"b">>,
+					<<"123">> => 123
+				}
+			},
+			json_parser:decode(
+				<<"
+				{
+					\"abc\" : [ \"def\", true, false, null, [], {\"a\":true}, 0, 0.768],
+					\"-*-\" : {
+						\"a\"   :    \"b\",
+						\"123\" : 123
+					}
+				}
+				">>
+			)
+		),
+		?_assertEqual(	
+			#{
+				<<"a">> => #{<<"a">> => #{<<"a">> => #{<<"a">> => null}}},
+				<<"b">> => [[[[0.56799]]]],
+				<<"c">> => [#{<<"c">> => [#{<<"c">> => false}]}]
+ 			},
+			json_parser:decode(
+				<<"
+				{
+					\"a\": {\"a\": {\"a\": {\"a\": null}}},
+					\"b\": [[[[0.56799]]]],
+					\"c\": [{\"c\": [{\"c\": false}]}]
+				}
+				">>
+			)
+		),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"a">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{\"a\":}">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{\"a\":true,}">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{\"a\":true">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{\"a\":}">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{a:true}">>)),
+		?_assertEqual({error, badarg}, json_parser:decode(<<"{\"a\"  \"b\"}">>))
+	].
 
+json_parser_decode_as_proplist_test_() ->
+	[
+		?_assertEqual([], json_parser:decode(<<"{}">>, [{format, proplist}])),
+		?_assertEqual(	
+			[
+				{<<"a">>, <<"string">>},
+			  	{<<"b">>, true},
+				{<<"c">>, false},
+				{<<"d">>, null},
+				{<<"e">>, []},
+				{<<"f">>, []},
+				{<<"g">>, 234},
+				{<<"h">>, 56.50},
+				{<<"i">>, <<>>},
+				{<<"j">>, 0}
+			],
+			json_parser:decode(
+				<<"
+				{
+					\"a\":\"string\",
+					\"b\":true,
+					\"c\":false,
+					\"d\":null,
+					\"e\":[],
+					\"f\":{},
+					\"g\":234,
+					\"h\":56.50,
+					\"i\": \"\",
+					\"j\": 0
+				}
+				">>
+			,[{format, proplist}])
+		),
+		?_assertEqual(	
+			[
+				{<<"abc">>, [
+					<<"def">>, true, false, null, [], [{<<"a">>, true}], 0, 0.768
+				]},
+				{<<"-*-">>, [
+					{<<"a">>, <<"b">>},
+					{<<"123">>, 123}
+				]}
+			],
+			json_parser:decode(
+				<<"
+				{
+					\"abc\" : [ \"def\", true, false, null, [], {\"a\":true}, 0, 0.768],
+					\"-*-\" : {
+						\"a\"   :    \"b\",
+						\"123\" : 123
+					}
+				}
+				">>
+			,[{format, proplist}])
+		),
+		?_assertEqual(	
+			[
+				{<<"a">>, [{<<"a">>, [{<<"a">>, [{<<"a">>, null}]}]}]},
+				{<<"b">>, [[[[0.56799]]]]},
+				{<<"c">>, [[{<<"c">>, [[{<<"c">> , false}]]}]]}
+ 			],
+			json_parser:decode(
+				<<"
+				{
+					\"a\": {\"a\": {\"a\": {\"a\": null}}},
+					\"b\": [[[[0.56799]]]],
+					\"c\": [{\"c\": [{\"c\": false}]}]
+				}
+				">>
+			,[{format, proplist}])
+		),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"a">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{\"a\":}">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{\"a\":true,}">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{\"a\":true">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{\"a\":}">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{a:true}">>, [{format, proplist}])),
+		?_assertEqual({error, badarg},
+			json_parser:decode(<<"{\"a\"  \"b\"}">>, [{format, proplist}]))
+	]. 
